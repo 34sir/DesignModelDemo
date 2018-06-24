cf85e21cef326b0fd1da8f20083ccb9423703969
chukaicheng <kaicheng.chu@sojex.cn>
2018-06-24 21:55:59 +0800

    完善代理模式

--- app/src/main/java/com/example/ckc/designmodeldemo/decoration/Decorator.java
+++ app/src/main/java/com/example/ckc/designmodeldemo/decoration/Decorator.java
@@ -1,7 +1,7 @@
 package com.example.ckc.designmodeldemo.decoration;
 
 //抽象装饰者
-public class Decorator extends Component {
+public abstract class Decorator extends Component {
     private Component component;//持有一个组件
 
     public Decorator(Component component) {
